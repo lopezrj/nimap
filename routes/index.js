6ded3b1dee930057c626c8776bd966d2dac06694
lopezrj <lopezrj@hotmail.com>
2017-04-03 13:56:07 -0600

    For several maps

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mapas de Nicaragua' });
 });
 
+/* GET map1 page. */
+router.get('/map1', function(req, res, next) {
+  res.render('map1', { title: 'Mapas de Nicaragua' });
+});
+
 module.exports = router;
